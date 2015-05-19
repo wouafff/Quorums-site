96916c5c7a2673e82a944263c43699eefcacc368
jb <jbaptiste.daniel@gmail.com>
2015-05-19 15:48:24 +0200

    changement -> majuscule à welcome et .com au lieu de .co

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE HTML>
+<!--
+	Miniport by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html ng-app>
+	<head>
+		<title>Quorums</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="politique décisionnel stratégie" />
+		<meta name="keywords" content="politique décisionnel stratégie campagne ONG" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.scrolly.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/init.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		</noscript>
+
+
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="container">
+			
+				</ul>
+			</nav>
+		<!-- Home -->
+			<div class="wrapper style1 first">
+				<article class="container" id="top">
+					<div class="row">
+						<div class="4u">
+							<span class="image fit"><img src="images/quorums4.png" alt="" /></span>
+						</div>
+						
+						<div class="8u">
+							<p><h4><a href="mailto:welcome@quorums.co" style="color:#707178">Welcome@quorums.com</a></h4></p>
+						
+						</div>
+
+					</div>
+				</article>
+			</div>
+		
+
+	</body>
+</html>
